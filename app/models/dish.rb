97e291e7a38c8f8cd1be966c505c48ad27f71d3e
Raghu Betina <raghu@firstdraft.com>
2018-09-25 21:08:03 +0000

    Indirect association between users and dishes

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -9,6 +9,10 @@ class Dish < ApplicationRecord
 
   # Indirect associations
 
+  has_many   :users,
+             :through => :bookmarks,
+             :source => :user
+
   # Validations
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ApplicationRecord
 
   # Indirect associations
 
+  has_many   :dishes,
+             :through => :bookmarks,
+             :source => :dish
+
   # Validations
 
   # Include default devise modules. Others available are:
